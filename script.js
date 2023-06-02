a0c8f5a2d3a8624c9c5587af19b40cd1f354d0db
jahangiralam85860 <jahangiralam85860@gmail.com>
2023-06-02 10:27:15 +0600

    submit

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+document.getElementById('btn-phone-plus').addEventListener('click', function () {
+    const phoneNumberField = document.getElementById('phone-number-field');
+    const phoneNumberString = phoneNumberField.value;
+    const priviousPhoneNumber = parseInt(phoneNumberString);
+
+    const newPhoneNumber = priviousPhoneNumber + 1;
+    phoneNumberField.value = newPhoneNumber;
+})
+
+
 document.getElementById('btn-case-plus').addEventListener('click', function () {
     const caseNumberField = document.getElementById('case-number-field');
     const caseNumberString = caseNumberField.value;
