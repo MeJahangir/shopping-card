876147fc409c44d453a5ea8bfb7ef31da0d5dcad
jahangiralam85860 <jahangiralam85860@gmail.com>
2023-08-26 17:10:04 +0600

    push

--- js&php/script.js
+++ js&php/script.js
@@ -1,2 +1,19 @@
 const jahangir = "I love you pripti";
 console.log(jahangir);
+
+// string length check
+let text = "QWERTYUIOPASDFGHJKLZXCVBNM";
+console.log(text.length);
+// result 26
+
+console.log(typeof 42)
+// result number 
+
+console.log(typeof 'jahangir');
+// result string 
+
+console.log(typeof true);
+// result boolean 
+
+console.log(typeof asdfg);
+// result undefined         
--- js&php/text.php
+++ js&php/text.php
@@ -1,3 +1,4 @@
 <?php
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
 
 ?>
